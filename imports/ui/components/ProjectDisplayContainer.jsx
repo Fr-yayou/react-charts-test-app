0a40c628396764c8046de4349b160a7f5a458da2
gurjit03 <gurjitmehta@gmail.com>
2017-02-06 19:18:17 +0530

    updated the package versions

--- imports/ui/components/ProjectDisplayContainer.jsx
+++ imports/ui/components/ProjectDisplayContainer.jsx
@@ -1,47 +1,46 @@
 import {Meteor} from 'meteor/meteor'
 import React,{Component} from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
-import { composeWithTracker } from 'react-komposer';
 import { Projects } from '../../api/projects/projects.js'
 import {ProjectDisplay} from './ProjectDisplay'
 
-export default class ProjectDisplayContainer extends Component {
-  render() {
-    return (
-      <div>
-        <h3>project display container........</h3>
-        {console.log(createContainer)}
-        </div>
-    );
-  }
-}
-// export default ProjectDisplayContainer = createContainer(({ params }) => {
-//     const subscription = Meteor.subscribe('project-data');
-//     const loading = !subscription.ready();
-//     let projects = Projects.find().fetch();
-//
-//     let filterData = function(searchInput,selectorName) {
-//       if(searchInput) {
-//         if(selectorName == 'hours' || selectorName == 'price') {
-//           projects = Projects.find({ selectorName : { $gt: searchInput } }).fetch();
-//         }else if(selectorName == 'course' || selectorName == 'classes') {
-//           console.log(Projects.find({selectorName : searchInput}).fetch());
-//           projects = Projects.find({selectorName : searchInput}).fetch();
-//         }else {
-//           const selector = {};
-//           selector.name = {
-//             $regex: new RegExp(`.*${searchInput}.*`, 'i')
-//           }
-//           projects = Projects.find(selector).fetch();
-//         }
-//       }
-//     }
-//
-//     console.log(params,"sdkjfslfjlj")
-//     console.log(projects);
-//
-//     return { loading, projects ,filterData};
-//   }, ProjectDisplay);
+// export default class ProjectDisplayContainer extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h3>project display container........</h3>
+//         {console.log(createContainer)}
+//         </div>
+//     );
+//   }
+// }
+export default ProjectDisplayContainer = createContainer(({ params }) => {
+    const subscription = Meteor.subscribe('project-data');
+    const loading = !subscription.ready();
+    let projects = Projects.find().fetch();
+
+    let filterData = function(searchInput,selectorName) {
+      if(searchInput) {
+        if(selectorName == 'hours' || selectorName == 'price') {
+          projects = Projects.find({ selectorName : { $gt: searchInput } }).fetch();
+        }else if(selectorName == 'course' || selectorName == 'classes') {
+          console.log(Projects.find({selectorName : searchInput}).fetch());
+          projects = Projects.find({selectorName : searchInput}).fetch();
+        }else {
+          const selector = {};
+          selector.name = {
+            $regex: new RegExp(`.*${searchInput}.*`, 'i')
+          }
+          projects = Projects.find(selector).fetch();
+        }
+      }
+    }
+
+    console.log(params,"sdkjfslfjlj")
+    console.log(projects);
+
+    return { loading, projects ,filterData};
+  }, ProjectDisplay);
 
 
 //
